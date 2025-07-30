c561cdecfa093780c9724ebfab80ff25c452033c
Aman Kumbhar <142677150+AK-r47@users.noreply.github.com>
2025-07-30 20:02:03 +0530

    Add files via upload

---
+++ conditional_statement.py
@@ -0,0 +1,17 @@
+n=int(input("Enter number="))
+# if n%2 == 0:
+#     print("EVEN")
+# else:
+#     print("ODD")12
+
+
+
+
+#-----------------------------------------------
+
+if(n%2 == 0):
+    print("is even")
+if n%2 != 0 and n%3 == 0:
+    print("is not even")
+else:
+    print("qwerty37")
