f1dbb8aa71107672ceff94c569c6bef79ffef0aa
Aman Kumbhar <142677150+AK-r47@users.noreply.github.com>
2025-07-30 19:39:30 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,22 @@
+a=3
+b=7
+print(a/b )
+print(a**b) 
+print(a+b)
+print(a%2)
+
+#---------------------------------------------------
+
+#assignment operators
+#  "=" "+="  "-=" * / %  
+a += 1
+print(a)
+
+#compatorss
+# "=="  "!="
+print(3==3)
+print(4!=3)
+
+#logical operators  
+print(3 == 3 and 4 == 7) #both condition true then only the output is true
+print("negation=",not 5==3)
