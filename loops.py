1667666365f0a4be2559c769ac3d2c4bb69f9e8b
Aman Kumbhar <142677150+AK-r47@users.noreply.github.com>
2025-07-31 14:42:57 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,61 @@
+# for i in range(5):
+#     print(i)
+#-----------------------------------------------------
+# sum=0
+# n=int(input("Enter number:"))
+# for i in range(n):
+#     sum += i+1
+#     print(sum)
+#-----------------------------------------------------
+
+
+# syntax will be :-
+#for iterator in seq:
+#statements
+#-------------------------------------------------------
+
+# i=2
+# i += 1 
+# print(i)
+
+#--------------------------------------------------------
+
+#(start,end,difference)
+# for z in range(1,10,2):
+#     print(z)
+
+#-----------------------------------------------------------
+# n=int(input("Enter input:"))
+
+# for i in range(n):
+#         if i%2 == 0:
+#             print("0", end=" ")  #use print on the single line 
+#         else:
+#             print("*",end=" ")
+
+#while condition 
+# i=0
+# while i<3:
+    
+#     print("hello")
+#     print(i) 
+#     i+=1
+#     print("hello2")
+
+#---------------------------------------------------------------------- -
+#while condition
+n=int(input())
+i=0
+while i<n:
+        if i%2 == 0:
+            print("0", end=" ")
+        else:
+            print("*" , end=" ")
+        i+=1
+
+
+
+
+
+
+
